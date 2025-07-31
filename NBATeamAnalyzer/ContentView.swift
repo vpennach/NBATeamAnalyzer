826ec89cdd792e47977df8d5f8c727b9f6f2ab80
Vincent Pennachio <vrp929@gmail.com>
2025-07-31 15:27:10 -0400

    Initial Commit

---
+++ NBATeamAnalyzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NBATeamAnalyzer
+//
+//  Created by Vincent Pennachio on 7/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NBATeamAnalyzer/NBATeamAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBATeamAnalyzerApp.swift
+//  NBATeamAnalyzer
+//
+//  Created by Vincent Pennachio on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct NBATeamAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
